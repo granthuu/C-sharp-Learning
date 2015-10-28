b1e8ac252c245e761a93c9a243ad4fb5665d4038
grant <zhuimengcanyang@163.com>
2015-10-28 16:00:26 +0800

    chapter-5: 多态。

--- source code/ConsoleApplication/ConsoleApplication/Program.cs
+++ source code/ConsoleApplication/ConsoleApplication/Program.cs
@@ -9,16 +9,20 @@ namespace ConsoleApplication
     {
         static void Main(string[] args)
         {
-            Horse horse = new Horse();
+            // Horse horse = new Horse();
+            Animal horse = new Horse();
             horse.Age = 1;
             Console.WriteLine("horse age is: {0}", horse.Age);
+            horse.Voice();
 
-            Sheep sheep = new Sheep();
+            // Sheep sheep = new Sheep();
+            Animal sheep = new Sheep();
             sheep.Age = 2;
             Console.WriteLine("sheep age is: {0}", sheep.Age);
+            sheep.Voice();
 
 
-            Console.WriteLine("hello world");
+            Console.WriteLine("\r\nhello world");
             Console.ReadKey();
         }
     }
@@ -38,17 +42,36 @@ namespace ConsoleApplication
                     _age = value;
             }
         }
+
+        // 对于每个动物的子类来说，每个子类发出的声音是不一样的
+        // 每个动物的子类需要对该方法进行重新定义：多态。
+        public virtual void Voice()
+        {
+            Console.WriteLine("animal voice.");
+        }
     }
 
     // 定义子类： 继承
+    // 子类应该重写基类的方法，每个子类发出的声音是不一样的。
     public class Horse : Animal
     { 
-        
+        // 通过override关键字来重写父类方法
+        public override void Voice()
+        {
+            // 调用基类的方法
+            base.Voice();
+            Console.WriteLine("horse voice .....");
+        }
     }
 
+    // 子类：重写方法
     public class Sheep : Animal
-    { 
-    
+    {
+        public override void Voice()
+        {
+            base.Voice();
+            Console.WriteLine("sheep voice ........");
+        }
     }
 
 }
