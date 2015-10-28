35dfd900f4388ea8f477d01f5f468c1c90860be4
grant <zhuimengcanyang@163.com>
2015-10-28 13:04:10 +0800

    add new class

--- source code/ConsoleApplication/ConsoleApplication/Program.cs
+++ source code/ConsoleApplication/ConsoleApplication/Program.cs
@@ -9,8 +9,32 @@ namespace ConsoleApplication
     {
         static void Main(string[] args)
         {
+            Person st = new Person();
+            st.Name = "grant";
+            st.PrintString(st.Name);
+
             Console.WriteLine("hello world");
             Console.ReadKey();
         }
     }
+
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        // add attribute
+        public string Name
+        {
+            get { return name;  }
+            set { name = value; }
+        }
+
+        // add method
+        public void PrintString(string name)
+        {
+            Console.WriteLine("name: " + name);
+        }
+    }
+
 }
