57877c367a662c406be1eab2a01b8f67f76ab40c
who_who <zhuimengcanyang@163.com>
2015-10-29 22:11:18 +0800

    chapter-9: event.

--- source code/ConsoleApplication/ConsoleApplication/Program.cs
+++ source code/ConsoleApplication/ConsoleApplication/Program.cs
@@ -5,34 +5,63 @@ using System.Text;
 
 namespace ConsoleApplication
 {
-    class Program
+    public class Bridegroom
     {
+        // 定义委托事件
+        public delegate void MarryHandler(string msg);
+
+        // 使用自定义委托类型定义事件
+        public event MarryHandler MarryEvent;
+
+        // 发出事件
+        public void OnMarriageComing(string msg)
+        {
+            if (MarryEvent != null)
+            {
+                MarryEvent(msg);
+            }
+        }
+
         static void Main(string[] args)
         {
-            Program p = new Program();
-            p.Greeting("Lucy", p.EnglishGreeting);
-            p.Greeting("张三",  p.ChineseGreeting);
+            Bridegroom bridegroom = new Bridegroom();
 
-            Console.WriteLine("\r\nhello world");
+            // 实例化朋友对象
+            Friend friend1 = new Friend("张三");
+            Friend friend2 = new Friend("李四");
+            Friend friend3 = new Friend("王五");
+
+            bridegroom.MarryEvent += new MarryHandler(friend1.SendMessage);
+            bridegroom.MarryEvent += new MarryHandler(friend2.SendMessage);
+            //bridegroom.MarryEvent += new MarryHandler(friend3.SendMessage);
+
+            bridegroom.OnMarriageComing("朋友们，我结婚了，到时候准时参加！");
+            Console.WriteLine("---------------------------------------------");
+
+            bridegroom.MarryEvent -= new MarryHandler(friend2.SendMessage);
+            bridegroom.MarryEvent += new MarryHandler(friend3.SendMessage);
+
+            bridegroom.OnMarriageComing("朋友们，我结婚了。");
             Console.ReadKey();
         }
 
-        public delegate void GreetingDelegate(string name);
-
-        public void Greeting(string name, GreetingDelegate callback)
+        public class Friend
         {
-            callback(name);
+            public string Name;
+
+            public Friend(string name)
+            {
+                Name = name;
+            }
+
+            public void SendMessage(string msg)
+            {
+                Console.WriteLine(msg);
+
+                Console.WriteLine(this.Name + "收到了，到时候准时参加。");
+            }
         }
 
-        public void EnglishGreeting(string name)
-        {
-            Console.WriteLine("hello "+name);
-        }
-
-        public void ChineseGreeting(string name)
-        {
-            Console.WriteLine("你好 "+name);
-        }
     }
 }
 
