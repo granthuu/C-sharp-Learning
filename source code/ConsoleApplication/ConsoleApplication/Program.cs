dfca753af55a5a81ec3198c6dab7bacbfe388662
grant <zhuimengcanyang@163.com>
2015-10-29 15:47:19 +0800

    chapter-7: 委托的使用2。

--- source code/ConsoleApplication/ConsoleApplication/Program.cs
+++ source code/ConsoleApplication/ConsoleApplication/Program.cs
@@ -7,37 +7,31 @@ namespace ConsoleApplication
 {
     class Program
     {
-        // 1. 定义一个委托类型
-        delegate void MyDelegate(int para1, int para2);
-
         static void Main(string[] args)
         {
-            // 2. 声明委托变量
-            MyDelegate d;
-
-            // 3. 实体化委托变量
-            d = new MyDelegate(new Program().Add);
-
-            // 4. 委托变量直接调用
-            d(1, 2);
-
-            // 5. 间接利用方法调用委托定义的函数。委托相当于函数指针作用。
-            MyMethod(d);
+            Program p = new Program();
+            p.Greeting("Lucy", p.EnglishGreeting);
+            p.Greeting("张三",  p.ChineseGreeting);
 
             Console.WriteLine("\r\nhello world");
             Console.ReadKey();
         }
 
-        private void Add(int para1, int para2)
+        public delegate void GreetingDelegate(string name);
+
+        public void Greeting(string name, GreetingDelegate callback)
         {
-            int sum = para1 + para2;
-            Console.WriteLine("sum: "+sum);
+            callback(name);
         }
 
-        // 传入的参数为委托类型，相当于函数指针。
-        private static void MyMethod(MyDelegate mydelegate)
+        public void EnglishGreeting(string name)
         {
-            mydelegate(1, 2);
+            Console.WriteLine("hello "+name);
+        }
+
+        public void ChineseGreeting(string name)
+        {
+            Console.WriteLine("你好 "+name);
         }
     }
 }
