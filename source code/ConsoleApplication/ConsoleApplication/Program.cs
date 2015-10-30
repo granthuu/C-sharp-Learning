f1250d83c14a3f784d2b753150205484c7bf86c1
grant <zhuimengcanyang@163.com>
2015-10-30 10:35:39 +0800

    chapter-10: 值类型按值传递

--- source code/ConsoleApplication/ConsoleApplication/Program.cs
+++ source code/ConsoleApplication/ConsoleApplication/Program.cs
@@ -5,64 +5,28 @@ using System.Text;
 
 namespace ConsoleApplication
 {
-    public class Bridegroom
+    public class Program
     {
-        // 定义委托事件
-        public delegate void MarryHandler(string msg);
-
-        // 使用自定义委托类型定义事件
-        public event MarryHandler MarryEvent;
-
-        // 发出事件
-        public void OnMarriageComing(string msg)
+        public static void Main(string[] args)
         {
-            if (MarryEvent != null)
-            {
-                MarryEvent(msg);
-            }
-        }
+            int num = 2;
+            Program program = new Program();
+            program.AddOn(num);
 
-        static void Main(string[] args)
-        {
-            Bridegroom bridegroom = new Bridegroom();
+            Console.WriteLine("before, num: " + num);
 
-            // 实例化朋友对象
-            Friend friend1 = new Friend("张三");
-            Friend friend2 = new Friend("李四");
-            Friend friend3 = new Friend("王五");
-
-            bridegroom.MarryEvent += new MarryHandler(friend1.SendMessage);
-            bridegroom.MarryEvent += new MarryHandler(friend2.SendMessage);
-            //bridegroom.MarryEvent += new MarryHandler(friend3.SendMessage);
-
-            bridegroom.OnMarriageComing("朋友们，我结婚了，到时候准时参加！");
-            Console.WriteLine("---------------------------------------------");
-
-            bridegroom.MarryEvent -= new MarryHandler(friend2.SendMessage);
-            bridegroom.MarryEvent += new MarryHandler(friend3.SendMessage);
-
-            bridegroom.OnMarriageComing("朋友们，我结婚了。");
             Console.ReadKey();
         }
 
-        public class Friend
+        private void AddOn(int num)
         {
-            public string Name;
-
-            public Friend(string name)
-            {
-                Name = name;
-            }
-
-            public void SendMessage(string msg)
-            {
-                Console.WriteLine(msg);
-
-                Console.WriteLine(this.Name + "收到了，到时候准时参加。");
-            }
+            num = num + 2;
+            Console.WriteLine("after, num: " + num);
         }
 
     }
+
+    
 }
 
 
